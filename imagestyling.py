cd256a6f6e531cde7c697cb2615f5f674e2e4838
karthiknair <33588934+karthiknair@users.noreply.github.com>
2018-11-10 17:39:25 +0530

    Add files via upload

--- imagestyling.py
+++ imagestyling.py
@@ -5,6 +5,14 @@ from PIL import ImageDraw
 img=Image.open('/home/pi/Desktop/hfresh/apple.jpg')
 font_type=ImageFont.truetype("/home/pi/Downloads/arial.ttf",72)
 draw=ImageDraw.Draw(img)
-draw.text(xy=(500,750),text='HELLO',fill=(0,0,0), font=font_type)
+x, y= (25,1500)
+text='APPLE Actual price= ▲5$, Discount Price= ▼2$'
+
+w, h= font_type.getsize(text)
+
+draw.rectangle((x,y, x+w, y+h), fill='black')
+draw.text((x,y),text='APPLE  Price= ▼2$',fill=(209,239,8),font=font_type)
+
+
 img.save('output2.jpg')
 img.show()
